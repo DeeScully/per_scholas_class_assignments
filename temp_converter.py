507cbcd3d24bacf2c4b53a625fea3d2376fd3352
wchu <wlanchu@gmail.com>
2017-02-11 13:26:55 -0500

    updated temp_converter and temp_chart

--- temp_converter.py
+++ temp_converter.py
@@ -1,7 +1,6 @@
 while True:
 
-    print('Hullo, I\'m Tempe, the temperature converter.')
-    fahr_temp = int(input('Please input the temperature in Fahrenheit that you would like to have converted to Centigrade: '))
+    fahr_temp = int(input('Hullo, I\'m Tempe, the temperature converter.  Please input the temperature in Fahrenheit that you would like to have converted to Centigrade: '))
     print('You inputted %sF, that is %dC!' %(fahr_temp, (fahr_temp - 32) / (9/5)))
     dump = input('Would you like to exit?')
     if dump.lower() == 'yes':
--- temperature_chart.py
+++ temperature_chart.py
@@ -1,2 +1,2 @@
-for i in range(0, 100, 5):
+for i in range(0, 101, 5):
     print('%sC is %dF' %(i, i * 9/5 + 32))
